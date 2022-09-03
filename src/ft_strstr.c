7677fde7d057c043d18c2554e9ad2aa0924beda9
Martijn Weverling <mweverli@student.codam.nl>
2022-09-04 01:25:30 +0200

    added ft_strstr.c

---
+++ src/ft_strstr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_strstr.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mweverli <mweverli@student.codam.n>          +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/09/04 01:21:08 by mweverli      #+#    #+#                 */
+/*   Updated: 2022/09/04 01:24:46 by mweverli      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	comp_str(const char *str, const char *ndl)
+{
+	int	i;
+
+	i = 0;
+	while ()
+}
+
+char	*ft_strstr(const char *hay, const char *ndl)
+{
+	int	i;
+	int	j;
+
+	if (!hay || !ndl)
+		return (NULL);
+}
