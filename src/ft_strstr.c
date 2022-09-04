34b517d83e67a52c24618b3576022deeb307172c
tentanus <tentanusw@gmail.com>
2022-09-04 18:57:36 +0200

    added ft_strstr.c; minor changes to ft_strtrim.c

--- src/ft_strstr.c
+++ src/ft_strstr.c
@@ -10,19 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "stddef.h"
+
 int	comp_str(const char *str, const char *ndl)
 {
 	int	i;
 
 	i = 0;
-	while ()
+	while (ndl[i])
+	{
+		if (str[i] != ndl[i])
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-char	*ft_strstr(const char *hay, const char *ndl)
+char	*ft_strstr(char *hay, const char *ndl)
 {
 	int	i;
-	int	j;
 
-	if (!hay || !ndl)
+	i = 0;
+	if (!hay)
 		return (NULL);
+	if (!(*ndl))
+		return (hay);
+	while (hay[i])
+	{
+		if (hay[i] == ndl[0])
+			if (comp_str(&hay[i], ndl))
+				return (&hay[i]);
+		i++;
+	}
+	return (NULL);
 }
--- src/ft_strtrim.c
+++ src/ft_strtrim.c
@@ -14,22 +14,22 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	i;
-	size_t	j;
+	size_t	begin;
+	size_t	end;
 	char	*dst;
 
-	i = 0;
-	while (ft_strchr(set, s1[i]) && *s1 != '\0')
-		i++;
-	j = ft_strlen(s1);
-	while (ft_strchr(set, s1[j - 1]) && j > i)
-		j--;
-	if (i > j)
+	begin = 0;
+	while (ft_strchr(set, s1[begin]))
+		begin++;
+	end = ft_strlen(s1);
+	while (ft_strchr(set, s1[end - 1]) && end > begin)
+		end--;
+	if (begin > end)
 		return (ft_strdup(""));
-	dst = (char *)malloc(sizeof(char) * (j - i + 1));
+	dst = (char *)malloc(sizeof(char) * (end - begin + 1));
 	if (!dst)
 		return (NULL);
-	ft_strlcpy(dst, &s1[i], (j - i + 1));
-	dst[j - i] = '\0';
+	ft_strlcpy(dst, &s1[begin], (end - begin + 1));
+	dst[end - begin] = '\0';
 	return (dst);
 }
