8cfb3ecd3983b8ffa0e504d46cc982f72ec7f79b
Martijn Weverling <mweverli@student.codam.nl>
2023-03-08 18:16:36 +0100

    LIBFT V3.0.2 ft_strjoin_fs1: free(s1) if malloc fails

--- src/string/ft_strjoin_fs1.c
+++ src/string/ft_strjoin_fs1.c
@@ -6,7 +6,7 @@
 /*   By: mweverli <mweverli@codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/13 11:30:21 by mweverli      #+#    #+#                 */
-/*   Updated: 2023/03/07 19:46:13 by mweverli      ########   odam.nl         */
+/*   Updated: 2023/03/08 16:06:03 by mweverli      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ char	*ft_strjoin_fs1(char *s1, char const *s2)
 	ls2 = ft_strlen(s2);
 	dst = malloc(sizeof(char) * (ls1 + ls2 + 1));
 	if (!dst)
-		return (NULL);
+		return (free(s1), NULL);
 	ft_strlcpy(dst, s1, (ls1 + 1));
 	ft_strlcat(dst, s2, (ls1 + ls2 + 1));
 	free(s1);
